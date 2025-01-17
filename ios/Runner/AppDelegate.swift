a2b9e7815588a16e7b66f4ad443fb907d15f6589
MukhametzhanDev <mukhametzhan.tileubek@gmail.com>
2025-01-17 15:55:51 +0500

    removed plugin yandex_map

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import Flutter
 import UIKit
-import YandexMapsMobile
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,8 +7,6 @@ import YandexMapsMobile
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    YMKMapKit.setLocale("ru_RU")
-    YMKMapKit.setApiKey("7b27f1d3-41c1-4ecb-9965-29145a9b1264")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
